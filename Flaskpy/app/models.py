b293a885494b3a7e133aa0a0089c4149ff395091
HD_HOME <137590629@QQ.COM>
2017-08-16 22:13:07 +0800

    添加文章,软那件Model

--- Flaskpy/app/models.py
+++ Flaskpy/app/models.py
@@ -4,6 +4,9 @@
 Name:HD
 '''
 from . import  db
+r'''
+    登陆用户
+'''
 class LoginUser(db.Model):
     __tablename__='loginuser'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -14,14 +17,18 @@ class LoginUser(db.Model):
     userHeadImg = db.Column(db.String(255),default='default.gif')
     loginCount = db.Column(db.Integer,default=0)
     validationEmail = db.Column(db.Integer,default=0)#是否验证邮箱,默认都是为验证
-
+r'''
+    用户登录的IP记录
+'''
 class LoginUserIp(db.Model):
     __tablename__='loginuserip'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     LoginIp = db.Column(db.String(18),default='0.0.0.0')
     LoginTime = db.Column(db.String(36))
     userId = db.Column(db.Integer,db.ForeignKey('loginuser.id'))
-
+r'''
+    菜单
+'''
 class SystemMenu(db.Model):
         __tablename__="systemMenu"
         id = db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -41,7 +48,47 @@ class SystemMenu(db.Model):
         是否启用,默认启用
         '''
         menuDisable = db.Column(db.Boolean,default=1)
+r'''
+    用户文章
+'''
+class UserArticle(db.Model):
+    __tablename__="UserArticle"
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    articleTitle = db.Column(db.String(512))
+    articleContent = db.Column(db.text)
+    insertTime = db.Column(db.DateTime)
+    insertUserId = db.Column(db.Integer,db.ForeignKey('loginuser.id'))
+    updateTime=db.Column(db.DateTime)
+    updateUserId = db.Column(db.Integer,db.ForeignKey('loginyser.id'))
+    menuDisable = db.Column(db.Boolean,default=1)
 
 
+r'''
+操作记录
 
+'''
+class OperationRecord(db.Model):
+    __tablename__="OperationRecord"
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    recordType=db.Column(db.String(32))
+    recordTime= db.Column(db.DateTime)
+    r'''
+        文章/软件的ID
+    '''
+    columnId = db.Column(db.Integer)
+
+
+r'''
+用户软件
+'''
+class UserSoftware(db.Model):
+    __tablename__="UserSoftware"
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    softwareName = db.Column(db.String(512))
+    softwareAddress = db.Column(db.String(1024))
+    insertTime = db.Column(db.DateTime)
+    insertUserId = db.Column(db.Integer, db.ForeignKey('loginuser.id'))
+    updateTime = db.Column(db.DateTime)
+    updateUserId = db.Column(db.Integer, db.ForeignKey('loginyser.id'))
+    menuDisable = db.Column(db.Boolean, default=1)
 
